3e2f204fbf0b81b5b8b4e15e2f8c88f5c13d4d2c
Maciej <maciej@myx.systems>
2020-09-26 20:06:50 +1000

    Initial Commit

--- Engineers/FirstViewController.swift
+++ Engineers/FirstViewController.swift
@@ -12,9 +12,85 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Initialise data loading and styliing of background elements
+        backgroundArea.layer.cornerRadius = 14
+        secondBackgroundArea.layer.cornerRadius = 14
+        loadData()
     }
+    
+    // Connections
+    @IBOutlet weak var backgroundArea: UIView!
+    @IBOutlet weak var secondBackgroundArea: UIView!
+    @IBOutlet weak var firstField: UITextField!
+    @IBOutlet weak var secondField: UITextField!
+    @IBOutlet weak var thirdField: UITextField!
+    @IBOutlet weak var fourthField: UITextField!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var dateSelector: UIDatePicker!
+    
+    @IBOutlet weak var firstSwitch: UISwitch!
+    @IBOutlet weak var secondSwitch: UISwitch!
+    @IBOutlet weak var thirdSwitch: UISwitch!
+    @IBOutlet weak var fourthSwitch: UISwitch!
+    @IBOutlet weak var FifthSwitch: UISwitch!
 
-
+    
+    // Functions
+    /// Gather text field data input by patients
+    func gatherData() -> String {
+        
+        var serial = ""
+        if firstSwitch.isOn {
+            serial = "Your Serial: " + firstField.text! + "\n"
+        } else {
+            serial = "Your Serial: " + "Secret" + "\n"
+        }
+        
+        var location = ""
+        if secondSwitch.isOn {
+            location = "Implant Location: " + secondField.text! + "\n"
+        } else {
+            location = "Implant Location: " + "Secret" + "\n"
+        }
+        
+        var date = ""
+        if thirdSwitch.isOn {
+            date = "Procedure Date: " + dateSelector.date.description + "\n"
+        } else {
+            date = "Procedure Date: " + "Secret" + "\n"
+        }
+        
+        var type = ""
+        if fourthSwitch.isOn {
+            type = "Procedure Type: " + thirdField.text! + "\n"
+        } else {
+            type = "Procedure Type: " + "Secret" + "\n"
+        }
+        
+        var note = ""
+        if FifthSwitch.isOn {
+            note = "Notes: " + fourthField.text! + "\n"
+        } else {
+            note = "Notes: " + "Secret" + "\n"
+        }
+        
+        let text = serial + location + date + type + note
+        return text
+    }
+    
+    /// Load text field data into a summary field
+    func loadData() {
+        summaryLabel.text = gatherData()
+    }
+    
+    // Actions
+    @IBAction func fieldEdited(_ sender: Any) {
+        loadData()
+    }
+    @IBAction func bgTapped(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        loadData()
+    }
 }
-
--- Engineers/SecondViewController.swift
+++ Engineers/SecondViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 MYX Systems. All rights reserved.
 //
 
+import NaturalLanguage
+import CoreML
 import UIKit
 
 class SecondViewController: UIViewController {
@@ -13,7 +15,22 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do { let mlModel = try engineersModel(configuration: MLModelConfiguration()).model
+            
+            let sentimentPredictor = try NLModel(mlModel: mlModel)
+            
+            let test = sentimentPredictor.predictedLabel(for: "my back hurts")
+            
+            print(test)
+            
+        } catch {
+        }
+    
+        
     }
+
+
+    
 
 
 }
